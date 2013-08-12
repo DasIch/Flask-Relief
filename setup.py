d8e712c8dda7053d81a8786b960b933612cbe653
Daniel Neuhäuser <dasdasich@gmail.com>
2013-08-12 19:06:43 +0200

    Add packages=['flask_relief'] to setup()

--- setup.py
+++ setup.py
@@ -25,5 +25,6 @@ setup(
     url='https://github.com/DasIch/Flask-Relief',
     install_requires=['Flask>=0.10', 'Relief>=0.2.0'],
     tests_require=['pytest>=2.3.5'],
-    cmdclass={'test': PyTest}
+    cmdclass={'test': PyTest},
+    packages=['flask_relief']
 )
