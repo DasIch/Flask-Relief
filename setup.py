6b478e9336ef5569369ee9fb50292ec4e519cd28
Daniel Neuhäuser <dasdasich@gmail.com>
2013-08-12 19:05:08 +0200

    Use Relief instead of relief in install_requires
    "Relief" is what is the exact PyPI package name.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author='Daniel Neuhäuser',
     author_email='ich@danielneuhaeuser.de',
     url='https://github.com/DasIch/Flask-Relief',
-    install_requires=['Flask>=0.10', 'relief>=0.2.0'],
+    install_requires=['Flask>=0.10', 'Relief>=0.2.0'],
     tests_require=['pytest>=2.3.5'],
     cmdclass={'test': PyTest}
 )
